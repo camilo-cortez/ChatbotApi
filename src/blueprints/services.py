2ccc2496d6eb0fabfdea0039645c4df68bc49c90
c.cortez <c.cortez@uniandes.edu.co>
2024-10-27 21:20:29 -0500

    Add getsolutions endpoint (PFM 27 terminada)

--- src/blueprints/services.py
+++ src/blueprints/services.py
@@ -2,11 +2,12 @@ from flask import Blueprint, request, jsonify
 from src.commands.ping import Ping
 from src.commands.chatbot import GetNode
 from src.errors.errors import BadRequest, NotFound
+from src.commands.solutions import GetIncidentSolutions
 
 services_bp = Blueprint('services', __name__)
 
 @services_bp.route('/getnode', methods=['GET'])
-def create_client():
+def get_node():
     try:
         json_input = request.get_json()
         command = GetNode(json_input)
@@ -18,6 +19,15 @@ def create_client():
         return jsonify({'error': str(e)}), 404
     except Exception as e:
         return jsonify({'error': 'Internal server error'}), 500
+    
+@services_bp.route('/getsolutions', methods=['GET'])
+def get_solutions():
+    try:
+        json_input = request.get_json()
+        command = GetIncidentSolutions(json_input)
+        return jsonify(command.execute()), 200
+    except Exception as e:
+        return jsonify({'error': e}), 500
 
 @services_bp.route('/ping', methods=['GET'])
 def ping():
